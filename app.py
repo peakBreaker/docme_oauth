644a62c5f35311664cdc2580c638d99993967870
Anders L. Hurum <andershurum@gmail.com>
2018-11-12 15:46:14 +0100

    enh: add org scope & change assert to condition

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.wsgi_app = ProxyFix(app.wsgi_app)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", "supersekrit")
 app.config["GITHUB_OAUTH_CLIENT_ID"] = os.environ.get("GITHUB_OAUTH_CLIENT_ID")
 app.config["GITHUB_OAUTH_CLIENT_SECRET"] = os.environ.get("GITHUB_OAUTH_CLIENT_SECRET")
-github_bp = make_github_blueprint()
+github_bp = make_github_blueprint(scope='read:org')
 app.register_blueprint(github_bp, url_prefix="/login")
 
 
@@ -19,15 +19,19 @@ def index():
     if not github.authorized:
         return redirect(url_for("github.login"))
     resp = github.get("/user")
-    assert resp.ok
     ret = '<h2>USER DETAILS:</h2><br>'
-    ret += "You are @{login} on GitHub<br>".format(login=resp.json()["login"])
+    if resp.ok:
+        ret += "You are @{login} on GitHub<br>".format(login=resp.json()["login"])
+    else:
+        ret += 'FAILED AT GETTING USER DATA<br>'
 
     # GET orgs
     resp = github.get("/user/orgs")
-    assert resp.ok
     ret += '<h2>ORGANIZATIONS:</h2><br>'
-    ret += "<pre>%s</pre>" % (json.dumps(resp.json(), indent=4))
+    if resp.ok:
+        ret += "<pre>%s</pre>" % (json.dumps(resp.json(), indent=4))
+    else:
+        ret += 'FAILED AT GETTING USERORG DATA<br>'
     return ret
 
 
